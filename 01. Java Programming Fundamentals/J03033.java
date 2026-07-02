f6462ee472ff1fbe332bc9be318df2b484b9e6ba
minhqng <minhnq.181005@gmail.com>
2026-07-02 23:48:42 +0700

    Solve J03033 — BỘI SỐ CHUNG NHỎ NHẤT

---
+++ 01. Java Programming Fundamentals/J03033.java
@@ -0,0 +1,18 @@
+//J03033 — BỘI SỐ CHUNG NHỎ NHẤT
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class J03033 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            BigInteger m = sc.nextBigInteger();
+            BigInteger n = sc.nextBigInteger();
+
+            System.out.println(m.multiply(n).divide(m.gcd(n)));
+        }
+    }
+}
