af49bfff80a2687fc93ee395daa7aebf0acbaf38
minhqng <minhnq.181005@gmail.com>
2026-07-02 19:00:43 +0700

    Solve J01006 - TÍNH SỐ FIBONACCI

---
+++ 01. Java Programming Fundamentals/J01006.java
@@ -0,0 +1,25 @@
+//J01006 — TÍNH SỐ FIBONACCI
+
+import java.util.Scanner;
+
+public class J01006 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long[] f = new long[93];
+        f[1] = 1;
+        f[2] = 1;
+
+        for (int i = 3; i <= 92; i++) {
+            f[i] = f[i - 1] + f[i - 2];
+        }
+
+        if (sc.hasNextInt()) {
+            int t = sc.nextInt();
+            while (t-- > 0) {
+                int n = sc.nextInt();
+                System.out.println(f[n]);
+            }
+        }
+    }
+}
