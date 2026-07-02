fcb91be7558358d2c14fd848572b7a8d19db2f91
minhqng <minhnq.181005@gmail.com>
2026-07-02 19:00:20 +0700

    Solve J01005 - CHIA TAM GIÁC

---
+++ 01. Java Programming Fundamentals/J01005.java
@@ -0,0 +1,20 @@
+//J01005 — CHIA TAM GIÁC
+
+import java.util.Scanner;
+
+public class J01005 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int h = sc.nextInt();
+
+            for (int i = 1; i < n; i++) {
+                System.out.printf("%.6f ", (h * Math.sqrt(1.0 * i / n)));
+            }
+            System.out.println();
+        }
+    }
+}
