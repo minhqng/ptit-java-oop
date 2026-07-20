2ad258efb726a7299d445de4511576f5ac410dc4
minhqng <minhnq.181005@gmail.com>
2026-07-20 23:08:08 +0700

    Solve J02019 — TỔNG ƯỚC SỐ - 2

---
+++ 02. Arrays and Matrices/J02019.java
@@ -0,0 +1,24 @@
+//J02019 — TỔNG ƯỚC SỐ - 2
+import java.util.*;
+
+public class J02019 {
+    public static final int N=(int)1e6+5;
+    public static int[] a=new int[N];
+
+    public static void dem(){
+        for(int i=1;i<=N-5;++i)
+            for(int j=i*2;j<=N-5;j+=i) a[j]+=i;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        dem();
+        int l=sc.nextInt();
+        int r=sc.nextInt();
+        int ans=0;
+        for(int i=l;i<=r;++i)
+            if (a[i]>i) ans++;
+        System.out.println(ans);
+        sc.close();
+    }
+}
