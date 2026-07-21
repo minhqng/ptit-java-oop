5f7222cb20887c305bcf0ae97ab6afa2d6b0f7b1
minhqng <minhnq.181005@gmail.com>
2026-07-21 19:17:36 +0700

    Solve J03014 — TỔNG SỐ NGUYÊN LỚN - 2

---
+++ 03. String Manipulations/J03014.java
@@ -0,0 +1,12 @@
+//J03014 — TỔNG SỐ NGUYÊN LỚN - 2
+import java.math.BigInteger;
+import java.util.*;
+
+public class J03014 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        BigInteger a=new BigInteger(sc.next());
+        BigInteger b=new BigInteger(sc.next());
+        System.out.println(a.add(b));
+    }
+}
