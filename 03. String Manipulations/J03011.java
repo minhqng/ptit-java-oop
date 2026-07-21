454fdeb7c143d3ebb097c5a8d8639f0bd8296775
minhqng <minhnq.181005@gmail.com>
2026-07-21 15:41:09 +0700

    Solve J03011 — ƯỚC SỐ CHUNG LỚN NHẤT CỦA SỐ NGUYÊN LỚN

---
+++ 03. String Manipulations/J03011.java
@@ -0,0 +1,15 @@
+//J03011 — ƯỚC SỐ CHUNG LỚN NHẤT CỦA SỐ NGUYÊN LỚN
+import java.math.*;
+import java.util.*;
+
+public class J03011 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0){
+            BigInteger a=new BigInteger(sc.next());
+            BigInteger b=new BigInteger(sc.next());
+            System.out.println(a.gcd(b));
+        }
+    }
+}
