3593827b871d3af6c604b944170955411ce1a72b
minhqng <minhnq.181005@gmail.com>
2026-07-21 19:18:06 +0700

    Solve J03015 — HIỆU SỐ NGUYÊN LỚN - 2

---
+++ 03. String Manipulations/J03015.java
@@ -0,0 +1,12 @@
+//J03015 — HIỆU SỐ NGUYÊN LỚN - 2
+import java.math.*;
+import java.util.*;
+
+public class J03015 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        BigInteger a=new BigInteger(sc.next());
+        BigInteger b=new BigInteger(sc.next());
+        System.out.println(a.subtract(b));
+    }
+}
